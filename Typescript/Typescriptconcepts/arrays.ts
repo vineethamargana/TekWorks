493b634dbdd592186920632f8d52c76d7acca9c2
vineethamargana <167873792+vineethamargana@users.noreply.github.com>
2024-11-21 19:10:13 +0530

    Delete Typescript directory

--- Typescript/Typescriptconcepts/arrays.ts
+++
@@ -1,6 +0,0 @@
-var values:string[]=["hi","hello","hjk"];
-var values1:Array<string>;
-values1=["jsk","hjk"];
-
-var values2:(string|number)[][]=[[10,20],["ghj","fghjkl"]];
-console.log(values2);
--- Typescript/Typescriptconcepts/tuple.js
+++
@@ -1,6 +0,0 @@
-var hii = [345, "ghj", true];
-console.log(hii);
-var hel = [[345, "ghj", true], [567, "vgbhn", false]];
-console.log(hel);
-console.log(hel[0]);
-console.log(hel[1]);
